f80e37bfa66a40a0399ff580cb4fcf051b30a24a
Aishwarya Rana <160381307+Aishwarya2004Github@users.noreply.github.com>
2024-06-19 10:06:51 +0530

    commit

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,26 +1,15 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import Logo from "../../assets/logo.svg";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 import Form from "./Form";
 
 const LoginPage = () => {
   const theme = useTheme();
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-  const handleWrongInput = () => {
-    toast.error("Invalid username or password!", {
-      position: "top-right",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  };
+
   return (
     <Box>
-      <ToastContainer />
+     
       <Box
         width="100%"
         backgroundColor={theme.palette.background.alt}
@@ -52,7 +41,7 @@ const LoginPage = () => {
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
           Welcome to Business Top, the Social Media for Business!
         </Typography>
-        <Form handleWrongInput={handleWrongInput} />
+
         <Form />
       </Box>
     </Box>
